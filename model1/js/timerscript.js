b5860265c8883937cc6c798593d645ba1af90cfa
lalawow <lalawow@gmail.com>
2015-02-17 21:59:35 +0800

    report function added.

--- model1/js/timerscript.js
+++ model1/js/timerscript.js
@@ -66,17 +66,26 @@ function display5DigitsTime(minutes, seconds) {
 }
 
 var beginSpeechTimer;
+var speechTimer;
+var speechCount = new Array(0,0);
 
 function startSpeechTimer(qulifiedTime, mediumTime, warningTime, unqualifiedTime, displayID) {
-	var commonTimer = new timer();
+	speechTimer = new timer();
 	var backgroundColor = "";
 	console.log("timer start");
 	var secondsCounter = 0;
+
+	if (displayID==="tableTopicsTimerDisplay") {
+		speechCount[0]++;
+	} else {
+		speechCount[1]++;
+	}
+
 	beginSpeechTimer = setInterval(function() {
-		commonTimer.addSecond();
+		speechTimer.addSecond();
 		secondsCounter +=1;
-		var minutes = commonTimer.getMinutes();
-		var seconds = commonTimer.getSeconds();
+		var minutes = speechTimer.getMinutes();
+		var seconds = speechTimer.getSeconds();
 		document.getElementById(displayID).innerHTML = display5DigitsTime(minutes,seconds);
 		if (secondsCounter > unqualifiedTime) {
 			backgroundColor = "darkred";
@@ -89,12 +98,38 @@ function startSpeechTimer(qulifiedTime, mediumTime, warningTime, unqualifiedTime
 		}
 		document.body.style.background = backgroundColor;
 	},1000);
+	console.log("time stop");
 }
 
-function stopSpeechTimer() {
+function stopSpeechTimer(displayID) {
 	clearInterval(beginSpeechTimer);
+	//add time report	
+	var minutes = speechTimer.getMinutes();
+	var seconds = speechTimer.getSeconds();
+    var speechNumber = speechCount[0];
+    if (displayID==="psReport") speechNumber = speechCount[1];
+    var result = speakerName(speechNumber) + " speaker used "+ display5DigitsTime(minutes,seconds) + " ^_^";
+	//add element to the report part
+	var yy = document.getElementById(displayID);
+	var addnode = document.createElement("tr");
+	var node = document.createTextNode(result);
+	addnode.appendChild(node);
+	document.getElementById(displayID).appendChild(addnode);
 }
 
 function clearColor() {
 	document.body.style.background = "";
+}
+
+function speakerName(number) {
+	switch (number) {
+		case 1: return ("1st");
+		break;
+		case 2: return ("2nd");
+		break;
+		case 3: return ("3rd");
+		break;
+		default: return (number + "th");
+	}
+
 }
